ec92e95d4efaf5521eb4b925066a21f3d8c7d01f
Thiago Caiubi <thiago.caiubi@gmail.com>
2015-09-26 16:23:56 -0300

    Enable statsd deleteIdleStats option

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
     "port": 8125,
     "mgmt_port": 8126,
     "flushInterval": 10000, // 10 seconds
-    "backends": ["./backends/console"]
+    "backends": ["./backends/console"],
+    "deleteIdleStats": true
 }
